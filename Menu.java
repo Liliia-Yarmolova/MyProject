c4eb0ac53ca0638324681785a918b4ae9becf62c
Liliia-Yarmolova <44850744+Liliia-Yarmolova@users.noreply.github.com>
2018-12-17 17:20:42 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,10 +1,11 @@
 package BookingRestaurant;
 
-import java.util.ArrayList;
+import java.util.*;
+
 
 public class Menu {
-    private final String name;
-    private final ArrayList<Dish> dishes = new ArrayList<>();
+    private String name;
+    private List<Dish> dishes = new ArrayList<>();
 
     public Menu(String name) {
         this.name = name;
@@ -14,7 +15,13 @@ public class Menu {
         dishes.add(dish);
     }
 
+
     public Dish getCheapestDish() {
+        TreeSet<Dish> result = new TreeSet<>(dishes);
+        return result.first();
+    }
+
+    /*public Dish getCheapestDish() {
         double price = dishes.get(0).getPrice();
         Dish cheapDish = null;
         for (Dish dish : dishes) {
@@ -23,20 +30,22 @@ public class Menu {
             }
         }
         return cheapDish;
-    }
+    }*/
 
     public String getName() {
         return name;
     }
 
-    public ArrayList<Dish> getDishes() {
+    public List<Dish> getDishes() {
         return dishes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String toString() {
         String menuName = name;
         return menuName;
-
     }
-
 }
